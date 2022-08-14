af7dc98f8fff27f7b4d82fa3533c8063f421b3a2
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-14 09:59:11 +0500

    030

--- if03.py
+++ if03.py
@@ -12,15 +12,23 @@ def main(a,b,c):
         return a
     if a < b and a > c:
         return a
+    elif a==b or a==c:
+        return (a or b or c) 
     
     if b > a and b < c:
         return b
     if b < a and b > c:
         return b
+    elif b==a or b==c:
+        return (b or a or c)
+    
     
     if c > b and c < a:
         return c
     if c < b and c > a:
         return c
+    elif c==a or c==b:
+        return (c or a or b)
+
     return a ,b, c
-print(main(6,5,4))
+print(main(6,5,5))
