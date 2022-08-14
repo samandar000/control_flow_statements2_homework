c3c75ad965bbcfebdb02247e4fdeddfdaa12b0b9
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-14 10:37:20 +0500

    07

--- if07.py
+++ if07.py
@@ -13,4 +13,16 @@ def main(temp):
     Returns:
         str: return answer.
     """
-    return
+    if temp<0:
+        return 'Freezing'
+    if temp>=1 and temp < 10:
+        return 'Very Cold '
+    if temp> 10 and temp < 20:
+       return 'Cold '
+    if temp<21 and temp < 30:
+        return 'Normal '
+    if temp>31 and temp < 40:
+        return 'Hot '
+    if temp>40:
+        return 'Very Hot '
+print(main(1))
