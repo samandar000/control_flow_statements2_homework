25b119c9f09b579ad8a6d32331dbabe555d0c83f
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-14 09:37:05 +0500

    04

--- if04.py
+++ if04.py
@@ -7,4 +7,12 @@ def main(a,b):
     Returns:
         int: return answer.
     """
-    return
+    if a == b:
+        return (0)
+    if  a > b:
+        return a
+    elif b > a:
+        return b
+    
+    return a , b
+print(main(3,4))
