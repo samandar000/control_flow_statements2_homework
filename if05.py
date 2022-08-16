4364a44752b32a2e5e7c798b4f1f35af95549a12
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-16 10:03:15 +0500

    05

--- if05.py
+++ if05.py
@@ -13,16 +13,16 @@ def main(n):
     x5=n%100000//10000
     if x1 > x2 and x1 > x3 and x1 >x4 and x1 > x5:
         return x1
-    if x2 > x1:
+    elif x2 > x1:
         return x2
-    if x3 > x1:
+    elif x3 > x1:
         return x3
-    if x4 > x1:
+    elif x4 > x1:
         return x4
-    if x5 > x1:
+    elif x5 > x1:
         return x5
     if x1==x2 or x3 or x4 or x5:
         return x1
     
-print(main(12345))
+print(main(16345))
     
